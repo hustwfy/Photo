5f048e3222792e8cb1b62bc0a6060869be7eab6c
hustwfy <1195292910@qq.com>
2019-12-21 22:55:57 +0800

    添加项目文件。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Photo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
